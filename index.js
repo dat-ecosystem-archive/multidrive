9d9e289e27f2b7aa5a2fe5092551606ffff4906c
Julian Gruber <julian@juliangruber.com>
2017-05-05 08:36:51 +0200

    add debug support

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mapLimit = require('map-limit')
 var assert = require('assert')
+var debug = require('debug')('multidrive')
 
 module.exports = multidrive
 
@@ -16,6 +17,7 @@ function multidrive (store, createArchive, closeArchive, cb) {
     close: close
   }
 
+  debug('initialize')
   store.read(sink)
 
   function sink (err, data) {
@@ -23,10 +25,12 @@ function multidrive (store, createArchive, closeArchive, cb) {
     var values = Object.keys(data).map(function (key) {
       return JSON.parse(data[key])
     })
+    debug('found %s dats', values.length)
 
     mapLimit(values, 1, createArchive, function (err, _archives) {
       if (err) return cb(err)
       archives = _archives
+      debug('initialized')
       cb(null, drive)
     })
   }
@@ -36,20 +40,27 @@ function multidrive (store, createArchive, closeArchive, cb) {
   }
 
   function create (data, cb) {
+    debug('create archive data=%j', data)
     createArchive(data, function (err, archive) {
       if (err) return cb(err)
       var key = archive.key
+      var hexKey = key.toString('hex')
+      debug('archive created key=%s', hexKey)
 
       var duplicates = archives.filter(function (_archive) {
         return _archive.key === key
       })
       var duplicate = duplicates[0]
-      if (duplicate) return cb(null, duplicate, Boolean(duplicate))
+      if (duplicate) {
+        debug('archive duplicate key=%s', hexKey)
+        return cb(null, duplicate, Boolean(duplicate))
+      }
 
       var _data
       if (data) _data = JSON.stringify(data)
       store.write(key, _data, function (err) {
         if (err) return cb(err)
+        debug('archive stored key=%s', hexKey)
         archives.push(archive)
         cb(null, archive)
       })
@@ -58,6 +69,7 @@ function multidrive (store, createArchive, closeArchive, cb) {
 
   function close (key, cb) {
     if (Buffer.isBuffer(key)) key = key.toString('hex')
+    debug('close archive key=%s', key)
     var i = 0
     var archive = archives.find(function (archive, j) {
       if (archive.key.toString('hex') !== key) return
@@ -67,8 +79,10 @@ function multidrive (store, createArchive, closeArchive, cb) {
     if (!archive) return cb(new Error('could not find archive ' + key))
     closeArchive(archive, function (err) {
       if (err) return cb(err)
+      debug('archive closed key=%s', key)
       store.delete(key, function (err) {
         if (err) return cb(err)
+        debug('archive deleted key=%s', key)
         archives.splice(i, 1)
         cb(null, archive)
       })
